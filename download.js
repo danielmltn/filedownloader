1cb4cd009f4a8975c07b967b4a66fff63802fefe
Daniel Melton <danielmelton@Daniels-MacBook-Pro-3.local>
2020-02-03 16:09:02 -0500

    refactor includes some easier functions to use for help

--- download.js
+++ download.js
@@ -3,25 +3,32 @@ import 'regenerator-runtime/runtime'
 import { default as request } from 'request-promise-native'
 import { writeFileSync } from 'fs'
 
-// export function download(outputFilename = './download', writeTo = writeFileSync) {
-//     return async (options = {uri: '', encoding: null}, asyncFetcher = request.get) => {
-//         const fileBuffer = await asyncFetcher(options)
-//         writeTo(outputFilename, fileBuffer)
-//         return true
-//     }
-// }
-
-function download(uri, encoding = null) {
-    return async (client = request.get) => {
-        const buffer = await client({uri: uri, encoding: encoding})
+function download(file = {uri: '', encoding: null}) {
+    return async (retrieve = request.get) => {
+        const buffer = await retrieve(file)
         return buffer
     }
 }
-
+    
 function writeToLocal(name, buffer, writeTo = writeFileSync) {
     if(name && buffer) {
         writeTo(name, buffer)
+    } else {
+        throw Error('failed to provide a proper name and/or buffer')
     }
 }
 
-export { download, writeToLocal }
+const pipe = (file, download, fileName, writeLocation, requestClient, writeTo) => async () => {
+    const buffer = await download(file)(requestClient)
+    writeLocation(fileName, buffer, writeTo)
+}
+const copyFileToLocal = (file, fileName, requestClient, writeTo) => pipe(
+    file,
+    download,
+    fileName,
+    writeToLocal,
+    requestClient,
+    writeTo
+)()
+
+export { download, writeToLocal, copyFileToLocal }
--- download.test.js
+++ download.test.js
@@ -1,38 +1,39 @@
-import { download, writeToLocal } from './download'
+import { download, writeToLocal, copyFileToLocal } from './download'
 
 describe('fileDownloader can download desired file successfully', () => {
     const fileUrls = ['https://www.riversource.com/content/files/111661.PDF']
 
-    // test('downloadFile downloads a pdf', async() => {
-    //     const downloader = fileDownloader('./testPdfFile.PDF')
-    //     try {
-    //         const x = await downloader({uri: fileUrls[0], encoding: null})
-    //         expect(x).toBe(true)
-    //     } catch(e) {
-    //         throw new Error(e)
-    //     }
-    // })
+    test('download returns a file as a buffer', async() => {
+        const file = {uri: fileUrls[0], encoding: null}
+        const client = async() => {return new Buffer.from('data')}
+        try {
+            const buffer = await download(file)(client)
+            expect(Buffer.isBuffer(buffer)).toBe(true)
+        } catch(e) {
+            throw new Error(e)
+        }
+    })
 
-    // test('downloadFile downloads a pdf', async() => {
-    //     try {
-    //         const buffer = await download(fileUrls[0])()
-    //         expect(Buffer.isBuffer(buffer)).toBe(true)
-    //         writeToLocal('./testPdfFile.PDF', buffer)
-    //     } catch(e) {
-    //         throw new Error(e)
-    //     }
-    // })
+    test('writeToLocal writes data to location', () => {
+        const buffer = new Buffer.from('data')
+        try {
+            writeToLocal('test', buffer, () => {})
+        } catch(e) {
+            throw new Error(e)
+        }
+    })
+
+    test('writeToLocal throws error', () => {
+        expect(() => writeToLocal()).toThrowError('failed to provide a proper name and/or buffer')
+    })
 
     test('downloadFile downloads a pdf', async() => {
         try {
-            const mockWriteToLocal = (name) => console.log('writing ' + name)
+            const mockWriteToLocal = async () => {}
             const fileName = './testPdfFile.PDF'
-            const pipe = (arg1, arg2) => () => arg2(fileName, arg1(fileUrls[0]))
-            const copyFileToLocal = pipe(
-                download,
-                writeToLocal
-            )
-            copyFileToLocal()
+            const file = {uri: fileUrls[0], encoding: null}
+            const client = async() => {return new Buffer.from('data')}
+            copyFileToLocal(file, fileName, client, mockWriteToLocal)
         } catch(e) {
             throw new Error(e)
         }
